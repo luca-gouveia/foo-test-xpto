b11892f33904407faf604a9d570f997b2f8f88f8
Luca Gouveia <38117857+luca-gouveia@users.noreply.github.com>
2024-08-07 21:55:19 -0300

    DESAFIO | Ajusta leitura do runner

--- teste/src/main/java/org/example/Runner.java
+++ teste/src/main/java/org/example/Runner.java
@@ -11,31 +11,31 @@ import java.util.Scanner;
 
 public class Runner {
 
-    private static String scan() {
+    private static int scan() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Escolher Desafio (1-4):");
-        return scanner.nextLine();
+        return scanner.nextInt();
     }
 
     public static void run() {
-        String resposta = null;
+        int resposta = 0;
 
-        while (!Arrays.asList("1", "2", "3", "4").contains(resposta)) {
+        while (!Arrays.asList(1,2,3,4).contains(resposta)) {
             resposta = scan();
         }
 
-        if (resposta.equals("1")) {
+        if (resposta == 1) {
             System.out.println("--- Votos em relação ao total de eleitores ---");
             var votos = new VotosResolve(1000, 800, 150, 50);
             System.out.println(votos.validos());
             System.out.println(votos.brancos());
             System.out.println(votos.nulos());
-        } else if (resposta.equals("2")) {
+        } else if (resposta == 2) {
             System.out.println("--- Bubble Sort ---");
             var array = ListaAleatoria.listaInt(5);
             System.out.println(Arrays.toString(array));
             System.out.println("> " + Arrays.toString(BubbleSort.ordenar(array)));
-        } else if (resposta.equals("3")) {
+        } else if (resposta == 3) {
             System.out.println("--- Fatorial ---");
             Scanner scanner = new Scanner(System.in);
             System.out.println("calcular fatorial de:");
